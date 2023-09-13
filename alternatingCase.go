d7a327118002f22039af026f54ea0bebb309cc28
Marcel Jaworski <marcel.jaworski@ils-gmbh.net>
2023-09-13 15:30:36 +0200

    alternatingCase

---
+++ alternatingCase.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func ToAlternatingCase(str string) string {
+	// var result string = str
+	result := make([]rune, 0, len([]rune(str)))
+	for _, char := range str {
+		if char == unicode.ToUpper(char) {
+			// lower := unicode.ToLower(char)
+			result = append(result, unicode.ToLower(char))
+		} else {
+			upper := unicode.ToUpper(char)
+			result = append(result, upper)
+		}
+
+	}
+	return string(result)
+}
+
+func main() {
+	fmt.Println(ToAlternatingCase("hAlLo1 örnek iş 世界"))
+}
