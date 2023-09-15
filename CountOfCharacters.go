ab5a4500fc52e99e15551987d86a4bc2940bb049
Marcel Jaworski <marcel.jaworski@ils-gmbh.net>
2023-09-15 08:56:40 +0200

    count sum of char

---
+++ CountOfCharacters.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Tuple struct {
+	Char  string
+	Count int
+}
+
+func OrderedCount(text string) []Tuple {
+	counts := make([]Tuple, 0, len(text))
+	counted := ""
+	for _, r := range text {
+		if strings.Count(counted, string(r)) == 0 {
+			counts = append(counts, Tuple{Char: string(r), Count: strings.Count(text, string(r))})
+			counted += string(r)
+		}
+	}
+
+	return counts
+}
+
+func main() {
+	fmt.Println(OrderedCount("abracadabra"))
+}
