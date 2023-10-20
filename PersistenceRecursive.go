bb4ea57e8bc1d891286bc8d969140bf8cde82c07
Marcel Jaworski <marcel.jaworski@ils-gmbh.net>
2023-10-20 11:19:48 +0200

    refactored

--- PersistenceRecursive.go
+++ PersistenceRecursive.go
@@ -10,11 +10,11 @@ func Persistence(n int) int {
 	var count int
 	str := strings.Split(strconv.Itoa(n), "")
 	if len(str) >= 2 {
-		_, count = Multiply(str, count)
+		count = Multiply(str, count)
 	}
 	return count
 }
-func Multiply(s []string, count int) (int, int) {
+func Multiply(s []string, count int) int {
 	multiply, _ := strconv.Atoi(s[0])
 	for i := 1; i < len(s); i++ {
 		num, _ := strconv.Atoi(s[i])
@@ -23,10 +23,10 @@ func Multiply(s []string, count int) (int, int) {
 	count++
 	str := strings.Split(strconv.Itoa(multiply), "")
 	if len(str) >= 2 {
-		multiply, count = Multiply(str, count)
+		count = Multiply(str, count)
 	}
-	return multiply, count
+	return count
 }
 func main() {
-	fmt.Println(Persistence(39))
+	fmt.Println(Persistence(999))
 }
